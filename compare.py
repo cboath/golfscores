409aaf3d7c237b2344d59999a2f31a72acb9877c
cboath <cteater@gmail.com>
2018-11-05 16:38:38 -0600

    Done for the day

--- compare.py
+++ compare.py
@@ -1,6 +1,6 @@
 def scorecompare(scoresA, scoresB, numOfHoles):
     whatever = 0
-    for x in range(0,7):   
+    for x in range(0,numOfHoles):   
         if(scoresA[x] > scoresB[x]):
             whatever = whatever + 1
         elif(scoresA[x] < scoresB[x]):
--- starter.py
+++ starter.py
@@ -1,10 +1,23 @@
 from compare import scorecompare
+import sys
 
-a = [1,2,9,6,5,6,7]
-b = [2,3,4,5,6,7,8]
+a = [1,2,9,6,5,6,9,5]
+b = [2,3,4,5,6,7,8,4]
+leader = ''
 
-numOfHoles = 7
+if(len(a) != len(b)):
+    sys.exit("Score mismatch")
+
+numOfHoles = len(a)
 
 breaker = scorecompare(a, b, numOfHoles)
 
-print('The winner is leading by ', breaker)
+if(breaker > 0):
+    leader = 'Player A'
+elif(breaker < 0):
+    leader = 'Player B'
+else:
+    print('The score is tied')
+    sys.exit()
+    
+print(leader + ' is leading by ' + str(abs(breaker)))
